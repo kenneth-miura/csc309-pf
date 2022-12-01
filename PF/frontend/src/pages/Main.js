149b4a375ead69c2f6189b34af6d003cf0a7ebb2
Chloe Zhao <yichenesia@gmail.com>
2022-12-01 14:04:08 -0500

    Image instead

--- PF/frontend/src/pages/Main.js
+++ PF/frontend/src/pages/Main.js
@@ -1,26 +1,18 @@
 import * as React from 'react';
-import Navbar from '../components/Navbar'
-import ImageCarousel from '../components/Carousel'
-import Box from '@mui/material/Box';
-import { Container } from '@mui/system';
 import { Card } from '@mui/material';
 
 function Main() {
   return (
     <div 
-    style={{width: '100%', height: '100%'}}
+    style={{display: 'flex', width: '100%', height: '100%'}}
     className="MainPage">
-        <div class="Carousel" style={{position: "absolute", width: '100%', top: "50px"}}>
-            <ImageCarousel></ImageCarousel>
-        </div>
+        <Card style={{width: "100%", height: "60%", position: 'fixed', top: '0px'}}>
+            <img style={{objectFit: "cover", width: "100%", height: "100%"}} src={require("../images/gym.png")}></img>
+        </Card>
 
         <Card class="WelcomeText" style={{backgroundColor: "#d62828", position: "fixed", width: "100%", top: "467px"}}>
             <h1>WELCOME TO TORONTO FITNESS</h1>
         </Card>
-
-        <Container >
-           
-        </Container>
 
     </div>
   );
