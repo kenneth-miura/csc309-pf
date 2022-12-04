6fe45c6ec4e94c4a5a7c266c95a6f911d571241c
Chloe Zhao <yichenesia@gmail.com>
2022-12-04 14:41:43 -0500

    Added sample 'sub' paging

--- PF/frontend/src/router.jsx
+++ PF/frontend/src/router.jsx
@@ -3,9 +3,11 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import ErrorPage from './pages/Error'
 import LoginPage from './pages/Login';
+import TextSubPage from './pages/TestSubPage'
 
 import {
     createBrowserRouter,
+    createRoutesFromElements,
     RouterProvider,
     Route,
   } from "react-router-dom";
@@ -19,8 +21,11 @@ const router = createBrowserRouter([
     {
         path: "/login",
         element: <LoginPage></LoginPage>,
-        
-    }
+    },
+    {
+      path: "/login/studio",
+      element: <TextSubPage></TextSubPage>
+  }
 ]);
 
 export default router;
