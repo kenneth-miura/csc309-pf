0b16d1658525e8b575c788925e76d3af335479ca
cindychen1023 <110849989+cindychen1023@users.noreply.github.com>
2022-12-05 12:37:58 -0500

    Add router

---
+++ PF/frontend/src/router.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import App from './App';
+import ErrorPage from './pages/Error'
+import LoginPage from './pages/Login';
+import TestSubPage from './pages/TestSubPage'
+import RegisterForm from './pages/Register'
+
+import {
+    createBrowserRouter,
+    createRoutesFromElements,
+    RouterProvider,
+    Route,
+  } from "react-router-dom";
+
+const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <App></App>,
+      errorElement: <ErrorPage/>,
+    },
+    {
+        path: "/login",
+        element: <LoginPage></LoginPage>,
+    },
+    {
+      path: "/login/studio",
+      element: <TestSubPage></TestSubPage>
+  },
+  {
+    path: "/login/register",
+    element: <RegisterForm></RegisterForm>
+}
+]);
+
+export default router;
