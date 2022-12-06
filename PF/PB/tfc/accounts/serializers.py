0e7f8a47f6fd74232505e48da13bb7a71456386e
Chloe Zhao <yichenesia@gmail.com>
2022-12-05 21:10:37 -0500

    Minor bugfix for backend patch

--- PF/PB/tfc/accounts/serializers.py
+++ PF/PB/tfc/accounts/serializers.py
@@ -16,7 +16,6 @@ class TFCUserSerializer(serializers.ModelSerializer):
             username=validated_data['username'],
             first_name=validated_data['first_name'],
             last_name=validated_data['last_name'],
-            password=validated_data['password'],
             email=validated_data['email'],
             phone_number=validated_data['phone_number'],
             avatar=validated_data['avatar'],
@@ -27,7 +26,31 @@ class TFCUserSerializer(serializers.ModelSerializer):
         return user
 
     def update(self, instance, validated_data):
-        instance.set_password(validated_data['password'])
-        instance.save()
+        keys = validated_data.keys()
+        print(keys)
+
+        if 'username' in keys:
+            instance.username = validated_data['username']
+            instance.save()
+
+        if 'first_name' in keys:
+            instance.first_name = validated_data['first_name']
+            instance.save()
+
+        if 'last_name' in keys:
+            instance.last_name = validated_data['last_name']
+            instance.save()
+
+        if 'email' in keys:
+            instance.email = validated_data['email']
+            instance.save()
+
+        if 'phone_number' in keys:
+            instance.phone_number = validated_data['phone_number']
+            instance.save()
+
+        if 'password' in keys:
+            instance.set_password(validated_data['password'])
+            instance.save()
 
         return instance
