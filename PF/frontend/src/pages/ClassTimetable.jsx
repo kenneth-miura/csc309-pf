e453bce9edf006524f2b05c4f7837fd522685bc7
Chloe Zhao <yichenesia@gmail.com>
2022-12-08 17:06:16 -0500

    fixes

--- PF/frontend/src/pages/ClassTimetable.jsx
+++ PF/frontend/src/pages/ClassTimetable.jsx
@@ -1,10 +1,104 @@
-import React, { useEffect } from "react";
-import { Card, Box, Button } from "@mui/material";
+import React, { useEffect, useState } from "react";
+import {
+  Card,
+  Box,
+  Button,
+  FormControl,
+  Input,
+  TextField,
+  InputLabel,
+  TableFooter,
+} from "@mui/material";
+import { NavLink } from "react-router-dom";
 import Navbar from "../components/Navbar";
-import ProfileInfoCard from "../components/ProfileInfo";
+import StudioMap from "../components/StudioMap";
 import Cookies from "universal-cookie";
-import ErrorPage from "./Error";
+import PropTypes from "prop-types";
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
+import Typography from "@mui/material/Typography";
+import { Stack } from "@mui/system";
+import Pagination from "@mui/material/Pagination";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell, { tableCellClasses } from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import { styled } from "@mui/material/styles";
+import IconButton from "@mui/material/IconButton";
+import LaunchIcon from "@mui/icons-material/Launch";
+import Dialog from "@mui/material/Dialog";
+import DialogTitle from "@mui/material/DialogTitle";
+import DialogContent from "@mui/material/DialogContent";
+import DialogActions from "@mui/material/DialogActions";
+import CloseIcon from "@mui/icons-material/Close";
+import Grid from "@mui/material/Grid";
 import { redirect, useNavigate } from "react-router-dom";
+import ErrorPage from "../pages/Error";
+
+
+const BootstrapDialog = styled(Dialog)(({ theme }) => ({
+    "& .MuiDialogContent-root": {
+      padding: theme.spacing(2),
+    },
+    "& .MuiDialogActions-root": {
+      padding: theme.spacing(1),
+    },
+  }));
+  
+  function BootstrapDialogTitle(props) {
+    const { children, onClose, ...other } = props;
+  
+    return (
+      <DialogTitle sx={{ m: 0, p: 2 }} {...other}>
+        {children}
+        {onClose ? (
+          <IconButton
+            aria-label="close"
+            onClick={onClose}
+            sx={{
+              position: "absolute",
+              right: 8,
+              top: 8,
+              color: (theme) => theme.palette.grey[500],
+            }}
+          >
+            <CloseIcon />
+          </IconButton>
+        ) : null}
+      </DialogTitle>
+    );
+  }
+  
+  BootstrapDialogTitle.propTypes = {
+    children: PropTypes.node,
+    onClose: PropTypes.func.isRequired,
+  };
+
+
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+    [`&.${tableCellClasses.head}`]: {
+      backgroundColor: theme.palette.common.black,
+      color: theme.palette.common.white,
+    },
+    [`&.${tableCellClasses.body}`]: {
+      fontSize: 14,
+    },
+  }));
+  
+  const StyledTableRow = styled(TableRow)(({ theme }) => ({
+    "&:nth-of-type(odd)": {
+      backgroundColor: theme.palette.action.hover,
+    },
+    // hide last border
+    "&:last-child td, &:last-child th": {
+      border: 0,
+    },
+  }));
+
+
 
 function ClassTimetablePage() {
   const cookie = new Cookies();
@@ -19,6 +113,9 @@ function ClassTimetablePage() {
     }
   }, [navigate]);
 
+
+
+
   return (
     <div>
       <Navbar position="sticky" isNotHomePage={true}></Navbar>
@@ -32,7 +129,7 @@ function ClassTimetablePage() {
               alignItems: "center",
             }}
           >
-            
+        
           </Box>
         </div>
       )}
