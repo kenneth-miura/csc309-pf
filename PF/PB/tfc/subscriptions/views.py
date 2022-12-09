0331a20c3deabb3c58f5a21660fddb4c2194a06a
Kenneth Miura <kenneth.t.miura@gmail.com>
2022-12-09 11:09:44 -0500

    actually remove adding payment history

--- PF/PB/tfc/subscriptions/views.py
+++ PF/PB/tfc/subscriptions/views.py
@@ -174,18 +174,6 @@ class SubscriptionDetail(APIView):
                                                             context={'user': request.user})
             if subscription_serializer.is_valid():
                 subscription = subscription_serializer.save()
-                payment_method = self.request.user.payment_method
-
-                payment_amount = get_object_or_404(SubscriptionPlan,
-                                                pk=subscription.subscription_type.id).price
-
-                payment_history_serializer = PaymentHistorySerializer(
-                    data={"amount": payment_amount, "payment_method_id": payment_method.id, "user_id": request.user.pk})
-                if payment_history_serializer.is_valid():
-                    payment_history_serializer.save()
-                else:
-                    return Response(payment_history_serializer.errors,
-                                    status=status.HTTP_400_BAD_REQUEST)
                 return Response(subscription_serializer.data, status=status.HTTP_201_CREATED)
             return Response(subscription_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
@@ -216,18 +204,6 @@ class SubscriptionDetail(APIView):
 
         if subscription_serializer.is_valid():
             subscription = subscription_serializer.save()
-
-            payment_amount = get_object_or_404(SubscriptionPlan,
-                                               pk=subscription.subscription_type.id).price
-
-            payment_history_serializer = PaymentHistorySerializer(
-                data={"amount": payment_amount, "payment_method_id": request.user.payment_method.id, "user_id": request.user.pk})
-            if payment_history_serializer.is_valid():
-                payment_history_serializer.save()
-            else:
-                print("payment history bad request")
-                return Response(payment_history_serializer.errors,
-                                status=status.HTTP_400_BAD_REQUEST)
 
             return Response(subscription_serializer.data, status=status.HTTP_201_CREATED)
 
