d80f17d65357252c46b64f13676d26e0de5f2280
Kenneth Miura <kenneth.t.miura@gmail.com>
2022-12-09 10:21:52 -0500

    show current card number

--- PF/frontend/src/pages/Subscription/EditCard.jsx
+++ PF/frontend/src/pages/Subscription/EditCard.jsx
@@ -6,21 +6,60 @@ import {
   formatExpirationDate,
 } from "./utils";
 import dayjs from "dayjs";
+import { Box } from "@mui/system";
 
 import Cookies from "universal-cookie";
 import "react-credit-cards-2/es/styles-compiled.css";
+import { Typography } from "@mui/material";
 
-function getFullDate(concatenatedDate){
-  const split_dates = concatenatedDate.split("/")
-  const month = split_dates[0]
-  const day = split_dates[1]
-  return dayjs().year() + "-" + month + "-" + day
+function getFullDate(concatenatedDate) {
+  const split_dates = concatenatedDate.split("/");
+  const month = split_dates[0];
+  const day = split_dates[1];
+  return dayjs().year() + "-" + month + "-" + day;
 }
 
-function getFullCardNumber(rawCardNumber){
-  const combinedNumber = rawCardNumber.replace(/\s/g,'')
-  return parseInt(combinedNumber)
+function getFullCardNumber(rawCardNumber) {
+  const combinedNumber = rawCardNumber.replace(/\s/g, "");
+  return parseInt(combinedNumber);
 }
+
+const CurrentCardNumber = () => {
+  const [hiddenCardNum, setHiddenCardNum] = React.useState(0);
+
+  const cookie = new Cookies();
+  const accessToken = cookie.get("accessToken");
+  const bearer = "Bearer " + accessToken;
+  console.log({ bearer });
+
+  fetch("http://127.0.0.1:8000/subscriptions/payment_method/", {
+    method: "GET",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      Authorization: bearer,
+    },
+  })
+    .then(response => {
+      if (response.status !== 200) {
+        throw new Error(response.status);
+      }
+      return response.json();
+    })
+    .then(data => {
+      const cardNumber = data.card_number.toString();
+      console.log({cardNumber})
+      const lastFourNumbers = cardNumber.substr(cardNumber.length - 4);
+      setHiddenCardNum(lastFourNumbers);
+    })
+    .catch(error => console.error(error));
+
+  return (
+    <Box>
+      <Typography>Current Card number ends in <b>{hiddenCardNum} </b></Typography>
+    </Box>
+  );
+};
 
 export default class EditCard extends React.Component {
   //from https://codesandbox.io/s/react-credit-card-3pw31?file=/src/utils.js:0-1306
@@ -35,25 +74,26 @@ export default class EditCard extends React.Component {
     has_payment_method: false,
   };
 
-  resetInputFields(){
+  resetInputFields() {
     this.setState({
       number: "",
       name: "",
       expiry: "",
       cvc: "",
-  })
+    });
   }
 
-  uploadCardInfo(e, number, name, expiry, cvc) {
+  uploadCardInfo(e, number, name, expiry, cvc, has_payment_method) {
     e.preventDefault();
     alert("You have finished payment!");
     const cookie = new Cookies();
     const accessToken = cookie.get("accessToken");
     const bearer = "Bearer " + accessToken;
-    console.log({bearer})
+    console.log({ bearer });
+    const method = has_payment_method ? "PUT" : "POST"
 
     fetch("http://127.0.0.1:8000/subscriptions/payment_method/", {
-      method: "PUT",
+      method: method,
       headers: {
         Accept: "application/json",
         "Content-Type": "application/json",
@@ -70,6 +110,7 @@ export default class EditCard extends React.Component {
         if (response.status !== 201) {
           throw new Error(response.status);
         }
+        this.setState({has_payment_method: true})
       })
       .catch(error => console.error(error))
       .finally(() => {
@@ -135,8 +176,8 @@ export default class EditCard extends React.Component {
     const { name, number, expiry, cvc, focused, issuer, has_payment_method } =
       this.state;
 
-    const state = this.state
-    console.log({state})
+    const state = this.state;
+    console.log({ state });
 
     const EditCardText = () => <h1>Overwrite your payment details</h1>;
     const SubmitCardText = () => <h1>Enter your payment details</h1>;
@@ -145,6 +186,7 @@ export default class EditCard extends React.Component {
       <div key="Payment">
         <div className="App-payment">
           {has_payment_method ? <EditCardText /> : <SubmitCardText />}
+          {has_payment_method && <CurrentCardNumber/>}
           <h4>please input your information below</h4>
           <Card
             number={number}
@@ -156,7 +198,7 @@ export default class EditCard extends React.Component {
           />
           <form
             ref={c => (this.form = c)}
-            onSubmit={e => this.uploadCardInfo(e, number, name, expiry, cvc)}
+            onSubmit={e => this.uploadCardInfo(e, number, name, expiry, cvc, has_payment_method)}
           >
             <div className="form-group">
               <small>Name on card:</small>
